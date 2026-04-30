4e9c41cbda1ac12cafe40b0f7aa29fed94a26404
Rega <regavanhooi@gmail.com>
2026-04-30 12:57:15 +0200

    Spark loads data from kafka into PostgreSQL

--- ingestion/spark_load.py
+++ ingestion/spark_load.py
@@ -1,6 +1,7 @@
-import os
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType
+from pyspark.sql.functions import from_json, col
 
 def main():
     # Define database configuration and paths
@@ -10,40 +11,64 @@ def main():
         "password": "admin",
         "driver": "org.postgresql.Driver"
     }
-    FILE_PATH = "data/2019-Nov.csv"
     TARGET_TABLE = "raw_events"
-
-    # Verify file existence before initializing the environment
-    if not os.path.exists(FILE_PATH):
-        print(f"Error: Target file not found at {FILE_PATH}", file=sys.stderr)
-        sys.exit(1)
 
     # Initialize Spark session
     spark = SparkSession.builder \
         .appName("Ecommerce_Raw_Ingestion") \
-        .config("spark.jars.packages", "org.postgresql:postgresql:42.6.0") \
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.6.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
         .getOrCreate()
+    
+    spark.sparkContext.setLogLevel("WARN")
+
+    schema = StructType([
+        StructField("event_time", StringType(), True),
+        StructField("event_type", StringType(), True),
+        StructField("product_id", LongType(), True),
+        StructField("category_id", LongType(), True),
+        StructField("category_code", StringType(), True),
+        StructField("brand", StringType(), True),
+        StructField("price", DoubleType(), True),
+        StructField("user_id", LongType(), True),
+        StructField("user_session", StringType(), True)
+    ])
 
     try:
-        # Load CSV file
-        # inferSchema=True is computationally expensive but necessary for initial loading (Raw layer)
-        df = spark.read.csv(FILE_PATH, header=True, inferSchema=True)
+        df = spark.readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "kafka:29092") \
+            .option("subscribe", TARGET_TABLE) \
+            .option("startingOffsets", "earliest") \
+            .load()
+        
+        # Cast value to String and deserialize JSON
+        parsed_df = df.selectExpr("CAST(value AS STRING) as json_string") \
+            .select(from_json(col("json_string"), schema).alias("data")) \
+            .select("data.*")
 
-        # Write to database
-        # mode="overwrite" ensures idempotency - replaces the entire table on subsequent runs
-        df.write.jdbc(
-            url=DB_URL, 
-            table=TARGET_TABLE, 
-            mode="overwrite", 
-            properties=DB_PROPERTIES
-        )
+        def write_to_postgres(batch_df, batch_id):
+            # This function runs for each micro-batch of data arriving from Kafka
+            # Write data to the database in "append" mode
+            batch_df.write \
+                .jdbc(url=DB_URL, table=TARGET_TABLE, mode="append", properties=DB_PROPERTIES)
+            
+            count = batch_df.count()
+            if count > 0:
+                print(f"Saved batch with ID: {batch_id} containing {count} rows.")
+
+        query = parsed_df.writeStream \
+            .foreachBatch(write_to_postgres) \
+            .outputMode("append") \
+            .start()
+
+        query.awaitTermination()
 
     except Exception as e:
         print(f"Error during Spark execution: {e}", file=sys.stderr)
         sys.exit(1)
         
     finally:
-        # Proper session closure releases resources (JVM memory)
+        # Proper session closure releases resources
         spark.stop()
 
 if __name__ == "__main__":
