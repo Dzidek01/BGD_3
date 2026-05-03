36c673cdd53f1aba3727aa1936096f2094a2cb70
Rega <regavanhooi@gmail.com>
2026-05-03 16:16:10 +0200

    Updated docker-compose with apache airflow and updated orechstration

---
+++ orchestration/dags/main_flow.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'admin',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+}
+
+with DAG(
+    dag_id='ecommerce_big_data_pipeline',
+    default_args=default_args,
+    description='Automatyczny potok ETL: Kafka -> Spark -> Postgres -> dbt',
+    schedule=timedelta(hours=1),  # <--- TUTAJ ZMIENIONO NAZWĘ PARAMETRU
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=['ecommerce', 'big_data'],
+) as dag:
+
+    # Uruchamiamy producenta (ścieżka mapowana z Twojego Dockera jako /opt/airflow/project)
+    run_producer = BashOperator(
+        task_id='run_kafka_producer',
+        bash_command='python /opt/airflow/project/ingestion/kafka_producer.py'
+    )
+
+    # Uruchamiamy dbt
+    run_dbt = BashOperator(
+        task_id='run_dbt_transformations',
+        bash_command='cd /opt/airflow/project/dbt_ecommerce && dbt run --profiles-dir .'
+    )
+
+    run_producer >> run_dbt
